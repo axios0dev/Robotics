6bc1aee748a05284116bb0afb8fe4ed54626303a
axios0dev <elliott.tiver0@gmail.com>
2018-11-28 16:36:19 +1030

    Ultrasonic sensor

--- bluetoothkbd.py
+++ bluetoothkbd.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 import curses
+import os
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -19,12 +20,6 @@ GPIO.setup(18, GPIO.OUT) #IN2
 GPIO.setup(27, GPIO.OUT) #IN3
 GPIO.setup(22, GPIO.OUT) #IN4
 
-#IR Sensors
-#output for IR sensors
-GPIO.setup(8, GPIO.OUT) #VCC Sensor 1 Left side  (Power)
-GPIO.setup(9, GPIO.IN) #Left sensor obstace detection (Input)
-GPIO.setup(10,GPIO.OUT) #VCC sensor 2 Right side (Power)
-GPIO.setup(11, GPIO.IN) #Right sensor obstace detection (Input)
 
 #define movment functions
 def fwd(i):
@@ -125,11 +120,15 @@ def skid(i):
 	time.sleep(i)
 #Arrow Pad Control Of Robot
 #First RC Function Test
+
+#Bluetooth Keyboard Script
 def keypad():
-	#Curses setup for input
+	import curses
+	#Curses setup for keyboard input
 	screen = curses.initscr()
 	curses.noecho()
-	curses.cbreak()
+	curses.halfdelay(5)
+	#curses.cbreak()
 	screen.keypad(True)
 	#Option Read Out
 	screen.addstr("Use The Arrow Keys To Control Robot\n")
@@ -139,111 +138,32 @@ def keypad():
 		char = screen.getch()
 		#return to main menu 
 		if char == ord('q'):
-			curses.nocbreak()
-			screen.keypad(False)
-			curses.echo()
+			curses.nocbreak(); screen.keypad(False); curses.echo() 
 			curses.endwin()
-			optionselect()
+			os.system('clear')
 			break
-
-		elif char == ord('s'):
-			screen.addstr(3,0,' Skids!!')
-			skid(0.03)
-
-		elif char == curses.KEY_RIGHT:
-			screen.addstr(3,0,' Right\n')
+		elif char == ord('k'):
+			screen.addstr(3,0,'Skids!!')
+			skid(0.01)
+		elif char == ord('j'):
+			screen.addstr(3,0,'Right\n')
 			turnRIGHT(0.01)
-
-		elif char == curses.KEY_LEFT:
-			screen.addstr(3,0,' Left\n')
+		elif char == ord('l'):
+			screen.addstr(3,0,'Left\n')
 			turnLEFT(0.01)
-
-		elif char == curses.KEY_UP:
-			screen.addstr(3,0,' Forward\n')
-			fwd(0.01)
-
-		elif char == curses.KEY_DOWN:
-			screen.addstr(3,0,' Reverse\n')
+		elif char == ord('w'):
+			screen.addstr(3,0,'Forward\n')
+			fwd(0.01)		
+		elif char == ord('s'):
+			screen.addstr(3,0,'Reverse\n')
 			rev(0.01)
-
 		elif char == ord('a'):
-			screen.addstr(3,0,' Left Pivot\n')
+			screen.addstr(3,0,'Left Pivot\n')
 			pivotleft(0.01)
-
 		elif char == ord('d'):
-			screen.addstr(3,0,' Right Pivot\n')
-			pivotright(0.01)	
-		
-			
-
-#IR based obstacle avoidence desicsion tree
-def detect():
-	print("Auto Roam With Collision Detection Enabled")
-	print("Press 1 To Roam Autonomusly")
-	print("Press 2 To Stop")
-	print("To Quit Current Mode Press")
-	USRINPT = input("Select Mode: ")
-	#Go until obstace detected then reverse and turn
-	while True:
-		leftsensor = GPIO.input(9)
-		rightsensor = GPIO.input(11)
-		#if obstacle detected on left turn reverse and turn right
-
-		if(leftsensor == False):
-			print("obstacle on left")
-			stop(0.5)
-			rev(0.5)
-			pivotright(0.3)
-		#if obstacle is deteced on right reverse and turn left
-		elif(rightsensor == False):
-			print ("obstacle on right")
-			stop(0.5)
-			rev(0.5)
-			pivotleft(0.3)
-		#if obstacle detected on both sensors reverse and turn right
-		elif(leftsensor == False and rightsensor == False):
-			print("Obstacle on Left and Right sensors")
-			stop(0.5)
-			rev(0.5)
-			pivotright(0.3)
-		else:
-			fwd(0.5)
-	
-#Xbox controller override
-#def controller():
-	
-
-#Mode Select Menu
-def optionselect():
-	print("Robotic Rover Option Menu Selection")
-	print("Press 1 For Autonomus Obstacle Avoidence")
-	print("Press 2 For Skids")
-	print("Press 3 For Keyboard Control Mode")
-	print("Press 4 For Wireless Controller Mode")
-	print("Press 5 For Remote Motor Shutdown")
-	selection = input("Select Mode: ")
-	if selection == 1:
-		detect()
-	elif selection == 2:
-		skid(10)
-	elif selection == 5:
-		print("Cutting Motor Power")
-		while True:
-			stop(1)
-	elif selection == 3:
-		print("Control Robot With Arrow Pad")
-		keypad()
-
-	elif selection == 0:
-		fwd(0.5)
-		turnRIGHT(0.2)
-		fwd(0.5)
-		turnLEFT(0.2)
-		fwd(0.5)
-		GPIO.cleanup()
-
-#Main {};
-optionselect()
-
-
-
+			screen.addstr(3,0,'Right Pivot\n')
+			pivotright(0.01)
+		elif char == curses.ERR:
+			screen.addstr(3,0,'Idle\n')
+			stop(0.01)
+keypad()
