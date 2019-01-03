becb5d667db761fd7346ab11f518b94445c55ee6
axios0dev <elliott.tiver0@gmail.com>
2019-01-03 20:18:20 +1030

    xbox 360 controller functionality

--- btoothscript.sh
+++ btoothscript.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-cd home/pi/Documents/python
+#!/bin/sh
+cd /
+cd /home/pi/Robotics
 sudo python bluetoothkbd.py
-@reboot sleep 20 && /bin/echo -e 'connect 00:00:00:00:2A:7D \n' | bluetoothct1
 
