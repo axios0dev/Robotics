ddfb59405a93b88c927bf1860a070e4ef5ab6509
axios0dev <elliott.tiver0@gmail.com>
2023-09-29 20:28:16 +0930

    SensorController just needs reading debounce added

--- AxiosRoboticsRCv1_src/Modules/SensorSubsystem/SensorController.py
+++ AxiosRoboticsRCv1_src/Modules/SensorSubsystem/SensorController.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-from serial import Serial 
+
+
+from serial import Serial
+from threading import Thread, Event 
 from time import sleep
 
 SerialConnection = Serial('/dev/ttyAMA0', 9600, timeout=1)
@@ -11,32 +14,57 @@ def ClearSerialBuffers():
         SerialConnection.reset_input_buffer()
         SerialConnection.reset_output_buffer()
 
-def main():
+def SensorControllerRoutine(SensorThreadState):
     
     ClearSerialBuffers()
     SerialConnection.write(b"ON\n")   
     
-    sleep(1)
+    sleep(0.1)
     
     try:
-        while True:
-            sleep(0.02)
+        while SensorThreadState.is_set():
+               
+           
             
             if (SerialConnection.in_waiting > 0):
                 try:
-                    line = SerialConnection.readline().decode('utf-8').rstrip()
-                    print(line)
+                    SensorSerialData = SerialConnection.readline().decode('utf-8').rstrip()
+                    LeftSensorData,RightSensorData = SensorSerialData.split(",")
+                    LeftSensorData = int(LeftSensorData.strip("L:"))
+                    RightSensorData = int(RightSensorData.strip("R:"))
+                    
+                    
+                    print("Left val", LeftSensorData)
+                    print("Right val", RightSensorData)
+                    
                     
                 except UnicodeDecodeError:
-                    print("Buffer Data Error")
-                       
-        
+                    print("Serial Buffer Error")
+                    
+            sleep(0.02)  
+            
+             
      
     except KeyboardInterrupt:
             print("closting serial connection")
             SerialConnection.write(b"OFF\n")   
             SerialConnection.close() 
 
+
+def main():
+    SensorThreadStateEvent = Event()
+    SensorThreadStateEvent.set()
+    
+    Thread(target=SensorControllerRoutine, args=(SensorThreadStateEvent,)).start()
+    
+    
+    sleep(10)
+    print("unsetting thread state")
+    SensorThreadStateEvent.clear()
+    
+    
+    
+    
 
 if __name__ == '__main__':
     main()
